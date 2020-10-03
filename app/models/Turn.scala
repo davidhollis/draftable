a6ed274473459e922743e28267debeb426397799
David Hollis <david@davidhollis.net>
2020-10-03 16:30:52 -0700

    Players can take turns

---
+++ app/models/Turn.scala
@@ -0,0 +1,24 @@
+package models
+
+import java.time.ZonedDateTime
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
+
+case class Turn(
+  id: Identifier[Turn],
+  name: String,
+  expires: Option[ZonedDateTime],
+  metadata: Map[String, String],
+) extends Identifiable[Turn]
+
+object Turn {
+  implicit val idPrefix: IdPrefix[Turn] = IdPrefix[Turn]("turn")
+
+  implicit val format: Format[Turn] = (
+    (__ \ implicitly[JsonConfiguration].naming("id")).format[Identifier[Turn]] and
+      (__ \ implicitly[JsonConfiguration].naming("name")).format[String] and
+      (__ \ implicitly[JsonConfiguration].naming("expires")).formatNullable[ZonedDateTime] and
+      (__ \ implicitly[JsonConfiguration].naming("metadata")).format[Map[String, String]]
+  )(Turn.apply, unlift(Turn.unapply))
+
+}
