419fab1a6ed4776f2f30e46c0acc3f57041c4ad7
David Hollis <david@davidhollis.net>
2020-10-03 13:26:40 -0700

    Drafts have players

---
+++ app/models/Player.scala
@@ -0,0 +1,21 @@
+package models
+
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
+
+case class Player(
+  id: Identifier[Player],
+  name: Option[String],
+) extends Identifiable[Player] {
+  def rename(newName: String): Player = copy(name = name orElse Some(newName))
+}
+
+object Player {
+  implicit val idPrefix: IdPrefix[Player] = IdPrefix[Player]("player")
+
+  implicit val format: Format[Player] = (
+    (__ \ implicitly[JsonConfiguration].naming("id")).format[Identifier[Player]] and
+      (__ \ implicitly[JsonConfiguration].naming("name")).formatNullable[String]
+  )(Player.apply, unlift(Player.unapply))
+
+}
