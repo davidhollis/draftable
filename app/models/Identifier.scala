508938d38900f4cafc2e5bbb5051afb4d8c92500
David Hollis <david@davidhollis.net>
2020-10-03 10:17:22 -0700

    Add notion of identifiers

---
+++ app/models/Identifier.scala
@@ -0,0 +1,61 @@
+package models
+
+import java.util.UUID
+import scala.util.Try
+import scala.util.matching.Regex
+import play.api.libs.json.{ JsError, JsString, JsSuccess, Reads, Writes }
+
+abstract class IdPrefix[T] {
+  val prefixString: String
+
+  @inline
+  override def toString(): String = prefixString
+
+}
+
+object IdPrefix {
+  def apply[T](prefix: String): IdPrefix[T] = new IdPrefix[T] { val prefixString: String = prefix }
+}
+
+class Identifier[T] private[Identifier] (
+  private val uuid: UUID
+)(
+  implicit
+  private val prefix: IdPrefix[T]
+) {
+  override lazy val toString: String = s"drn:${prefix}:${uuid}"
+
+  override def equals(obj: Any): Boolean =
+    obj match {
+      case id: Identifier[_] => id.toString == this.toString
+      case _                 => false
+    }
+
+}
+
+object Identifier {
+  val idRegex: Regex = """drn:(.+):([0-9a-f-]+)""".r.anchored
+  def apply[T]()(implicit prefix: IdPrefix[T]): Identifier[T] = new Identifier[T](UUID.randomUUID())
+
+  def applyOpt[T](idString: String)(implicit prefix: IdPrefix[T]): Option[Identifier[T]] =
+    for {
+      List(prefixString, uuidString) <- idRegex.unapplySeq(idString)
+      if prefixString == prefix.toString()
+      uuid <- Try(UUID.fromString(uuidString)).toOption
+    } yield new Identifier[T](uuid)
+
+  def unapply[T](id: Identifier[T]): Option[String] = Some(id.toString)
+
+  implicit def reads[T: IdPrefix]: Reads[Identifier[T]] =
+    Reads {
+      case JsString(str) =>
+        Identifier.applyOpt(str) match {
+          case Some(id) => JsSuccess(id)
+          case None     => JsError("Malformed identifier string")
+        }
+      case _ => JsError("Malformed identifier: must be a string")
+    }
+
+  implicit def writes[T: IdPrefix]: Writes[Identifier[T]] = Writes { id => JsString(id.toString) }
+
+}
